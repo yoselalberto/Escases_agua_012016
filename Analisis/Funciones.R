173b614283df95c8df239ed0a257cb635d56c48b
Alberto <yoselerratil@gmail.com>
2016-01-28 00:25:40 -0600

    nombre de las variables mas intuitivas

--- Analisis/Funciones.R
+++ Analisis/Funciones.R
@@ -1,8 +1,8 @@
 # Aquí están funciones utiles
-elim_Acent <- function(texto, mat_tran) {
+elim_Acent <- function(texto, matriz_trans) {
                  nom_trans <- texto
                  for (i in seq_len(nrow(caract_trans))) {
-                 nom_trans <-  str_replace_all(string = nom_trans, pattern = mat_tran[i, 1], replacement = mat_tran[i, 2])
+                 nom_trans <-  str_replace_all(string = nom_trans, pattern = matriz_trans[i, 1], replacement = matriz_trans[i, 2])
                  }
                  return(nom_trans)
               }

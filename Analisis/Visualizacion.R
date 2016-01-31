af1dc230df9378b542c31f9887c1c9109615bf9b
Alberto <yoselerratil@gmail.com>
2016-01-30 23:47:38 -0600

    creo una vesion interactiva con leaflet#

--- Analisis/Visualizacion.R
+++ Analisis/Visualizacion.R
@@ -4,11 +4,16 @@ library(sp)
 library(rgdal)
 library(magrittr)
 library(dplyr)
+library(leaflet)
+library(htmltools)
 
 # cargado de datos
 # mapa delagacional
 #delegaciones
 dir_shp <- "../Datos/Shapes"
+
+mapa_df <- readOGR(dsn = "../Datos/Shapes/DF",layer = "df_estatal") %>%
+           geometry()
 delegaciones <- readOGR(dsn = dir_shp,layer = "df_municipal") %>%
                 geometry()
 # todas las colonias
@@ -23,6 +28,20 @@ col_sin <- "#F03B20"
 col_poca <- "#FEB24C"
 
 # Mapas
+color_afec <- colonias_afectadas$color
+centro_col <- coordinates(colonias_afectadas)
+# leaflet
+mapa <- leaflet(data = colonias_afectadas) %>%
+        setView(-99.168, 19.383, zoom = 11) %>%
+        addProviderTiles("CartoDB.PositronNoLabels") %>%
+        setMaxBounds(-99.3649, 19.0482,-98.9403,19.5927) %>%
+        addPolygons(data = mapa_df, weigth = 5, color = "gray40", fillColor = NULL) %>%
+        addPolygons(data = delegaciones, weight = 2, color = "gray97", fillOpacity = 0.1) %>%
+        addPolygons(color = ~color, weight = 1, fillOpacity = 0.6, 
+                    popup = htmlEscape(~colonia))
+
+
+# en png
 png(filename = "../Imagenes/Colonias_efecto_1.png", width = 800, height = 900)
 par(mar = c(0, 0, 0, 0) + 0.1, xaxs = "i", yaxs = "i",  lwd = 0.15, cex = 2)
 plot(delegaciones, bg = "gray97")
